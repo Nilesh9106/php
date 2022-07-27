5b7f6734f3649c5483ae64a0835d0116b0917381
Nilesh darji <97864081+Nilesh9106@users.noreply.github.com>
2022-07-27 14:40:34 +0530

    Create lab4.sql

---
+++ lab4.sql
@@ -0,0 +1,33 @@
+create table sales_master(
+    salesman_no varchar(6) CONSTRAINT s_no PRIMARY KEY CHECK(salesman_no like 's%'),
+    sal_name varchar(20) NOT NULL,
+    address1 varchar(30) not null,
+    address2 varchar(30) not null,
+    city varchar(20) ,
+    state varchar(20),
+    pincode number(6),
+    sal_amt number(8,2) not null check(sal_amt > 0),
+    tgt_to_get number(6,2) not null check(tgt_to_get > 0),
+    ytd_sales number (6,2) not null ,
+    remarks varchar(60)
+);
+create table sales_order(
+    s_order_no VARCHAR2(6) PRIMARY KEY check(s_order_no like '0%'),
+    order_date DATE NOT NULL,
+    client_no varchar2(6) REFERENCES client_master(client_no),
+    dely_add varchar2(25),
+    salesman_no varchar2(6) REFERENCES sales_master(salesman_no),
+    dely_type char(1) default 'F' check(dely_type in ('F','P') ),
+    bill_yn char(1) ,
+    dely_date DATE,
+    order_status varchar2(10) ,
+    check(dely_date>order_date),
+    check(order_status in ('in process','fulfilled','back order','cancelled'))
+);
+CREATE TABLE sales_order_details(
+    s_order_no varchar2(6) REFERENCES sales_order(s_order_no),
+    product_no varchar2(6) REFERENCES product_master(product_no),
+    qty_order number(8) ,
+    qty_disp number(8) ,
+    product_rate number(10,2)
+);
